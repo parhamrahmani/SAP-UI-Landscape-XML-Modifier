56a19f8f7228a67c8ba5d300997cabfb1f6e3269
SPMovingT <parhamrahmani13762@gmail.com>
2023-06-09 10:42:56 +0200

    error handling

--- SAPUILandscape_UUID_Manipulator.py
+++ SAPUILandscape_UUID_Manipulator.py
@@ -2,13 +2,14 @@
 # as service id. I want to regenerate uuids of both and update service id of Item accordingly.
 import xml.etree.ElementTree as ET
 import uuid
+import os
 
 # Description of the program
 description = """
 SAP UI Landscape XML Modifier:
 This program allows you to modify SAP UI Landscape XML files by regenerating UUIDs for workspaces, 
 nodes, services, and items. It also provides the option to remove includes and rename workspaces, in order 
-make them usable as central files
+make them usable as central files.
 
 Note: Make sure to backup your XML file before making any modifications.
 
@@ -17,80 +18,93 @@ Note: Make sure to backup your XML file before making any modifications.
 print(description)
 
 # Prompt the user for the XML file path
-xml_file_path = input("Enter the XML file path: ")
+while True:
+    xml_file_path = input("Enter the XML file path: ").strip('"')
 
-# Load the XML file
-tree = ET.parse(xml_file_path)
-root = tree.getroot()
+    # Validate XML file path
+    if not os.path.isfile(xml_file_path):
+        print("Error: Invalid XML file path. Please try again.")
+    else:
+        break
 
-# Get the existing workspaces in the XML file
-workspaces = root.findall(".//Workspace")
+try:
+    # Load the XML file
+    tree = ET.parse(xml_file_path)
+    root = tree.getroot()
 
-# Show a list of workspaces
-print("List of Workspaces:")
-for index, workspace in enumerate(workspaces):
-    print(f"{index + 1}. {workspace.get('name')}")
+    # Get the existing workspaces in the XML file
+    workspaces = root.findall(".//Workspace")
 
-# Prompt the user for UUID regeneration for workspaces
-regenerate_all_uuids = input("Do you want to regenerate UUIDs for all workspaces? (y/n): ").lower() == "y"
+    # Show a list of workspaces
+    print("List of Workspaces:")
+    for index, workspace in enumerate(workspaces):
+        print(f"{index + 1}. {workspace.get('name')}")
 
-# Regenerate UUIDs for workspaces
-for index, workspace in enumerate(workspaces):
-    if regenerate_all_uuids or input(
-            f"Regenerate UUID for workspace '{workspace.get('name')}'? (y/n): ").lower() == "y":
-        workspace.set('uuid', str(uuid.uuid4()))
-        new_workspace_name = input(f"Enter the new name for workspace '{workspace.get('name')}': ")
-        workspace.set('name', new_workspace_name)
+    # Prompt the user for UUID regeneration for workspaces
+    regenerate_all_uuids = input("Do you want to regenerate UUIDs for all workspaces? (y/n): ").lower() == "y"
 
-# Regenerate UUIDs for Node elements
-for node in root.findall(".//Node"):
-    node.set('uuid', str(uuid.uuid4()))
+    # Regenerate UUIDs for workspaces
+    for index, workspace in enumerate(workspaces):
+        if regenerate_all_uuids or input(
+                f"Regenerate UUID for workspace '{workspace.get('name')}'? (y/n): ").lower() == "y":
+            workspace.set('uuid', str(uuid.uuid4()))
+            new_workspace_name = input(f"Enter the new name for workspace '{workspace.get('name')}': ")
+            workspace.set('name', new_workspace_name)
 
-# Create a mapping of old service IDs to new UUIDs
-service_mapping = {}
+    # Regenerate UUIDs for Node elements
+    for node in root.findall(".//Node"):
+        node.set('uuid', str(uuid.uuid4()))
 
-# Regenerate UUIDs for Service elements and update service IDs in Item elements
-for service in root.findall(".//Service"):
-    old_uuid = service.get('uuid')
-    new_uuid = str(uuid.uuid4())
-    service_mapping[old_uuid] = new_uuid
-    service.set('uuid', new_uuid)
+    # Create a mapping of old service IDs to new UUIDs
+    service_mapping = {}
 
-    # Update the serviceid in Item elements
-    for item in root.findall(".//Item[@serviceid='" + old_uuid + "']"):
-        item.set('serviceid', new_uuid)
+    # Regenerate UUIDs for Service elements and update service IDs in Item elements
+    for service in root.findall(".//Service"):
+        old_uuid = service.get('uuid')
+        new_uuid = str(uuid.uuid4())
+        service_mapping[old_uuid] = new_uuid
+        service.set('uuid', new_uuid)
 
-# Regenerate UUIDs for Item elements
-for item in root.findall(".//Item"):
-    item.set('uuid', str(uuid.uuid4()))
+        # Update the serviceid in Item elements
+        for item in root.findall(".//Item[@serviceid='" + old_uuid + "']"):
+            item.set('serviceid', new_uuid)
 
-# Check if there is an include with the URL containing "SAPUILandscapeGlobal.xml"
-includes = root.findall(".//Include")
-is_global_include_present = any("SAPUILandscapeGlobal.xml" in include.get("url") for include in includes)
+    # Regenerate UUIDs for Item elements
+    for item in root.findall(".//Item"):
+        item.set('uuid', str(uuid.uuid4()))
 
-if is_global_include_present:
-    removing_include = input("This XML file includes SAPUILandscapeGlobal.xml in it. In order to make this "
-                             "file into a central file, this inclusion has to be deleted. Do you "
-                             "want to delete it? (y/n): ").lower() == "y"
+    # Check if there is an include with the URL containing "SAPUILandscapeGlobal.xml"
+    includes = root.findall(".//Include")
+    is_global_include_present = any("SAPUILandscapeGlobal.xml" in include.get("url") for include in includes)
 
-    if removing_include:
-        filtered_includes = []
+    if is_global_include_present:
+        removing_include = input("This XML file includes SAPUILandscapeGlobal.xml in it. In order to make this "
+                                 "file into a central file, this inclusion has to be deleted. Do you "
+                                 "want to delete it? (y/n): ").lower() == "y"
 
-        # Remove includes with URLs containing "SAPUILandscapeGlobal.xml"
-        for include in includes:
-            include_url = include.get("url")
-            if "SAPUILandscapeGlobal.xml" not in include_url:
-                filtered_includes.append(include)
+        if removing_include:
+            filtered_includes = []
 
-        # Replace the include elements with the filtered list
-        root.findall(".//Includes")[0][:] = filtered_includes
-else:
-    print("No include with URL containing 'SAPUILandscapeGlobal.xml' found.")
+            # Remove includes with URLs containing "SAPUILandscapeGlobal.xml"
+            for include in includes:
+                include_url = include.get("url")
+                if "SAPUILandscapeGlobal.xml" not in include_url:
+                    filtered_includes.append(include)
 
-# Save the modified XML file with a unique name
-input_output_path = input("Enter the output path for the modified XML file: ")
-input_modified_name = input("Enter the name for your output file: ")
-output_file_name = input_output_path + '/' + input_modified_name + '.xml'
-tree.write(output_file_name, encoding='utf-8', xml_declaration=True)
+            # Replace the include elements with the filtered list
+            root.findall(".//Includes")[0][:] = filtered_includes
+    else:
+        print("No include with URL containing 'SAPUILandscapeGlobal.xml' found.")
 
-print("Modified XML file saved as:", output_file_name)
+    # Save the modified XML file with a unique name
+    input_output_path = input("Enter the output path for the modified XML file: ")
+    input_modified_name = input("Enter the name for your output file: ")
+    output_file_name = os.path.join(input_output_path, input_modified_name + '.xml')
+    tree.write(output_file_name, encoding='utf-8', xml_declaration=True)
+
+    print("Modified XML file saved as:", output_file_name)
+
+except Exception as e:
+    print("An error occurred while processing the XML file:", str(e))
+
+
