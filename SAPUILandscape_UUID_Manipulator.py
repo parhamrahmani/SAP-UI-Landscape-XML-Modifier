332b1e648328911bbc000114a53a08fc397d9f28
SPMovingT <parhamrahmani13762@gmail.com>
2023-06-14 17:50:57 +0200

    Issues Fixed -->
    1. All Services listed !
    2. sorted by the System Types
    3. finds duplicates
    4. All *important data* exported successfully

--- SAPUILandscape_UUID_Manipulator.py
+++ SAPUILandscape_UUID_Manipulator.py
@@ -113,7 +113,8 @@ def generate_excel_file(xml_file_path):
         'System Client': [],
         'System URL': [],
         'System Server': [],
-        'Router Address': []
+        'Router Address': [],
+        'Is Duplicate': []
     }
 
     # Iterate over the nodes
@@ -121,17 +122,17 @@ def generate_excel_file(xml_file_path):
         node_name = node.get('name')
 
         # Iterate over the items in the node
-        for item in node.findall('.//Item'):
-            service_id = item.get('serviceid')
+        for item_node in node.findall('.//Item'):
+            service_id = item_node.get('serviceid')
 
             # Iterate over Services
             for service in root.findall('.//Service'):
                 service_uuid = service.get('uuid')
-                if service_uuid == service_id:
+                service_type = service.get('type')
+                if service_uuid == service_id and service_type == 'SAPGUI':
                     service_name = service.get('name')
                     service_description = service.get('description')
                     service_sid = service.get('systemid')
-                    service_type = service.get('type')
                     service_client = service.get('client')
                     service_url = service.get('url')
                     service_server = service.get('server')
@@ -146,6 +147,8 @@ def generate_excel_file(xml_file_path):
                         if service_router == router_uuid:
                             router_address = router.get('router')
 
+
+
                     # Add the data to the dictionary
                     data_sheet_1['Node'].append(node_name)
                     data_sheet_1['System Name'].append(service_name)
@@ -156,43 +159,48 @@ def generate_excel_file(xml_file_path):
                     data_sheet_1['System URL'].append(service_url)
                     data_sheet_1['System Server'].append(service_server)
                     data_sheet_1['Router Address'].append(router_address)
+                    data_sheet_1['Is Duplicate'].append('')
 
-    # Find items that are not within any nodes
-    for item in root.findall('.//Item'):
-        # Get the item UUID
-        service_id = item.get('serviceid')
+    for service in root.findall('.//Service'):
+        servicetype = service.get('type')
+        if servicetype != 'SAPGUI':
+            servicename = service.get('name')
+            servicedescription = service.get('description')
+            servicesid = service.get('systemid')
+            serviceclient = service.get('client')
+            serviceurl = service.get('url')
+            serviceserver = service.get('server')
 
-        for service in root.findall('.//Service'):
-            service_uuid = service.get('uuid')
-            if service_id == service_uuid:
-                service_name_2 = service.get('name')
-                service_desc = service.get('description')
-                service_sid_2 = service.get('systemid')
-                service_type_2 = service.get('type')
-                service_client_2 = service.get('client')
-                service_url_2 = service.get('url')
-                service_server_2 = service.get('server')
+            # Initialize router_address with an empty string
+            routeraddress = ''
 
-                # Find the router based on the router ID
-                for router in root.findall('.//Router'):
-                    router_uuid = router.get('uuid')
-                    service_router = service.get('routerid')
-                    if service_router == router_uuid:
-                        router_address = router.get('router')
+            # Find the router based on the router ID
+            for router in root.findall('.//Router'):
+                router_uuid = router.get('uuid')
+                service_router = service.get('routerid')
+                if service_router == router_uuid:
+                    routeraddress = router.get('router')
 
-                        # Add the data to the dictionary
-                        data_sheet_1['Node'].append(' ')
-                        data_sheet_1['System Id'].append(service_sid_2)
-                        data_sheet_1['System Name'].append(service_name_2)
-                        data_sheet_1['System Description'].append(service_desc)
-                        data_sheet_1['System Type'].append(service_type_2)
-                        data_sheet_1['System Client'].append(service_client_2)
-                        data_sheet_1['System URL'].append(service_url_2)
-                        data_sheet_1['System Server'].append(service_server_2)
-                        data_sheet_1['Router Address'].append(router_address)
+            # Add the data to the dictionary
+            data_sheet_1['Node'].append('*Not included in a node*')
+            data_sheet_1['System Name'].append(servicename)
+            data_sheet_1['System Description'].append(servicedescription)
+            data_sheet_1['System Id'].append(servicesid)
+            data_sheet_1['System Type'].append(servicetype)
+            data_sheet_1['System Client'].append(serviceclient)
+            data_sheet_1['System URL'].append(serviceurl)
+            data_sheet_1['System Server'].append(serviceserver)
+            data_sheet_1['Router Address'].append(routeraddress)
+            data_sheet_1['Is Duplicate'].append('')
 
     # Create a DataFrame from the data dictionary
     df = pd.DataFrame(data_sheet_1)
+
+    # Mark duplicates in the 'System Server' column if system type is 'SAPGUI'
+    df.loc[df['System Type'] == 'SAPGUI', 'Is Duplicate'] = df.duplicated(subset='System Server')
+
+    # Mark duplicates in the 'System URL' column if system type is not 'SAPGUI'
+    df.loc[df['System Type'] != 'SAPGUI', 'Is Duplicate'] = df.duplicated(subset='System URL')
 
     # Save the DataFrame to Excel
     output_file_path = xml_file_path.replace('.xml', '.xlsx')
